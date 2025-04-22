ff3d42f80ee2571f4a6d5dd236b852579573d278
gwenlbt <glebreton001@ensc.fr>
2025-04-22 10:33:25 +0200

    Simulation

--- code_principal/BonneFee.cs
+++ code_principal/BonneFee.cs
@@ -6,7 +6,7 @@ public abstract class BonneFee
     public string Effet { get; set; }
     public int DureeTours { get; set; }
 
-    public BonneFee(string nom, string type, string cible, string effet,  int dureeTours) // constructeur
+    public BonneFee(string nom, string type, string cible, string effet, int dureeTours) // constructeur
     {
         Nom = nom;
         Type = type;
@@ -17,8 +17,8 @@ public abstract class BonneFee
 
     public override string ToString() // méthode pour afficher textuellement les obstacles et tout les informations qui les concernent
     {
-        return $"{Nom} ({Type}) - Cible : {Cible}, Effet : {Effet}, Gravité : {Gravite*100}%" +
-        $"Temporaire : {(EstTemporaire ? "Oui, " + DureeTours + "tours(s)" : "Non")}";
+        return $"{Nom} - ({Type}) - Cible : {Cible}, Effet : {Effet}, Gravité : {Gravite * 100}%" +
+        $"Temporaire : {(EstTemporaire ? "Oui, " + DureeTours + "tours(s)" : " ")}";
     }
 
 }
