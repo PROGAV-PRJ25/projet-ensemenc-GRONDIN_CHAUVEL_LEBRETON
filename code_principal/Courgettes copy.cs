af0c5a8f50c1ec7683f022d9236fd1d2598c453c
AbigaelleGRD <agrondin001@ensc.fr>
2025-05-05 14:15:16 +0200

    Suppression d'un .cs

--- code_principal/Courgettes copy.cs
+++
@@ -1,8 +0,0 @@
-public class Champignon: Legume
-{
-    public Champignon() : base("Courgette", "Eté", TypePlante.Legume = 1, "Limoneux", 30f, 19f, 7, 150, Sante.EnBonneSante = 0, 5) {}
-    public override int EtatFinal()
-    {
-        return 10;
-    }
-}
