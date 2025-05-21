ac1eb8bbb3bcdc88da0a70cfd30d8a8081bb6993
AbigaelleGRD <agrondin001@ensc.fr>
2025-05-21 17:55:26 +0200

    réglage de conflits

--- code_principal/Program.cs
+++ code_principal/Program.cs
@@ -1,16 +1,1 @@
-﻿﻿Tomate tomate = new Tomate();
-Console.WriteLine("=== Test de la plante Tomate ===");
-Console.WriteLine(tomate); // appelle ToString
-
-tomate.ExposerAuSoleil(); // hérité de Plante
-tomate.Fertiliser();      // hérité de Plante
-
-Console.WriteLine("Après soleil et fertilisation :");
-Console.WriteLine(tomate);
-
-// tester une méthode propre à Plante
-tomate.ArroserPlantes();
-tomate.MettreAJourCroissance();
-tomate.AfficherJauge();
-
- Console.WriteLine("=== Fin du test ===");
+﻿﻿
